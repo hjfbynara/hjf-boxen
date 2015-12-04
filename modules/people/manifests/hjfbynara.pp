582b00b4da6970119631de1db140dfe692bf7ab2
hjfbynara <hjferrara@bynara.com>
2015-12-03 20:19:33 -0500

    updated for my system

---
+++ modules/people/manifests/hjfbynara.pp
@@ -0,0 +1,89 @@
+class people::coreone {
+    include brewcask
+    include chrome
+    include cord
+#    include cyberduck
+    include docker_toolbox
+    include dockutil
+    include dropbox
+    include firefox
+#    include fish
+#    include fitbit
+    include googledrive
+    include hipchat
+#    include jumpcut
+#    include pivotalbooster
+#    include robomongo
+    include sequel_pro
+#    include shiftit
+#    include spotify
+    include sublime_text::v2
+    include vagrant
+    include wget
+
+    $sublimeConfDir = "/Users/${::luser}/Library/Application Support/Sublime Text 2"
+    $structure = [ "${sublimeConfDir}", "${sublimeConfDir}/Packages" ]
+
+    file { $structure:
+        ensure  => 'directory',
+        owner   => "${::luser}",
+        mode    => '0755'
+    }->
+    file { "${sublimeConfDir}/Packages/User/Preferences.sublime-settings":
+      content  => '
+{
+    "color_scheme": "Packages/Color Scheme - Default/Sunburst.tmTheme",
+    "font_size": 12.0,
+    "trim_trailing_white_space_on_save": true,
+    "tab_size": 4,
+    "translate_tabs_to_spaces": true,
+    "ignored_packages":
+    [
+        "Vintage"
+    ]
+}'
+    }
+
+    $dockitems = hiera_hash('dockitems', {})
+    create_resources('dockutil::item', $dockitems)
+
+    $rubygems = hiera_hash('rubygems', {})
+    create_resources('ruby_gem', $rubygems)
+
+    # Homebrew: Apps.....
+    package {[
+        'cowsay',
+        'docker',
+        'docker-machine',
+        'figlet',
+        'graphviz',
+        'netcat',
+        'vault'
+    ]:
+    }
+
+    # Homebrew: Cask (GUI) Apps.....
+    package {[
+#        'tinkertool',
+        'google-cloud-sdk',
+        'java',
+#        'seashore',
+    ]:
+        provider => 'brewcask',
+    }
+
+    include osx::global::enable_keyboard_control_access
+    include osx::finder::unhide_library
+    include osx::global::expand_save_dialog
+
+#    osx::dock::hot_corner { 'Bottom Right':
+#        action => 'Start Screen Saver'
+#    }
+#    osx::dock::hot_corner { 'Bottom Left':
+#        action => 'Disable Screen Saver'
+#    }
+
+#    class {'osx::global::natural_mouse_scrolling':
+#        enabled => false
+#    }
+}
